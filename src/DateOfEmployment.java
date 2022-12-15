ac950ef32687c5ac866f0d6483acb97e207bb374
Evgenygroup <mailfrom61@gmail.com>
2022-12-15 12:11:57 +0100

    lesson

--- src/DateOfEmployment.java
+++
@@ -1,7 +0,0 @@
-public class DateOfEmployment {
-    String month;
-    int date;
-    Manager manager;
-
-
-}
--- src/game/MainGame.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-public class MainGame {
-    public static void main(String[] args) {
-        GameGuessNumber game = new GameGuessNumber();
-        game.start();
-    }
-}
